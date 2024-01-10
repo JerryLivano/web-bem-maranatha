6746b4bf1c543cb3cdcfc3e354440c53e7ae6ccc
JerryLivano <2172002@maranatha.ac.id>
2024-01-10 07:35:59 +0700

    ganti form

--- resources/views/pendaftaran/index.blade.php
+++ resources/views/pendaftaran/index.blade.php
@@ -5,7 +5,7 @@
     <div class="card">
         <h3 class="card-header">Form Pendaftaran Kepengurusan BEM</h3>
         <div class="card-body">
-            <form>
+            <form enctype="multipart/form-data">
                 <div class="form-group">
                     <label for="name">Nama Lengkap:</label> <span class="text-danger">*</span>
                     <input type="text" class="form-control" name="name" id="name" placeholder="Input Nama" required>
@@ -100,32 +100,32 @@
                 </div>
                 <div class="form-group">
                     <label for="nilai">Transkrip Nilai:</label> <span class="text-danger">*</span>
-                    <input class="form-control" type="text" required name="nilai" id="nilai" placeholder="Link Transkrip Nilai">
+                    <input class="form-control" type="file" accept=".pdf" required name="nilai" id="nilai" placeholder="Link Transkrip Nilai">
                     <small id="fileHelp" class="form-text text-muted">Kirim dalam link google drive</small>
                 </div>
                 <div class="form-group">
                     <label for="cv">Curriculum Vitae:</label> <span class="text-danger">*</span>
-                    <input class="form-control" type="text" required name="cv" id="cv" placeholder="Link CV">
+                    <input class="form-control" type="file" accept=".pdf" required name="cv" id="cv" placeholder="Link CV">
                     <small id="fileHelp" class="form-text text-muted">Kirim dalam link google drive</small>
                 </div>
                 <div class="form-group">
                     <label for="rekomKetua">Surat Rekomendasi Ketua Organisasi:</label> <span class="text-danger">*</span>
-                    <input class="form-control" type="text" required name="rekomKetua" id="rekomKetua" placeholder="Link Surat Rekomendasi">
+                    <input class="form-control" type="file" accept=".pdf" required name="rekomKetua" id="rekomKetua" placeholder="Link Surat Rekomendasi">
                     <small id="fileHelp" class="form-text text-muted">Kirim dalam link google drive</small>
                 </div>
                 <div class="form-group">
                     <label for="osjur">Sertifikat Lulus Ospek Jurusan:</label> <span class="text-danger">*</span>
-                    <input class="form-control" type="text" required name="osjur" id="osjur" placeholder="Link Sertifikat Ospek Jurusan">
+                    <input class="form-control" type="file" accept=".pdf" required name="osjur" id="osjur" placeholder="Link Sertifikat Ospek Jurusan">
                     <small id="fileHelp" class="form-text text-muted">Kirim dalam link google drive</small>
                 </div>
                 <div class="form-group">
                     <label for="wiratha">Sertifikat/Surat Pernyataan Lulus Wiratha Fest:</label> <span class="text-danger">*</span>
-                    <input class="form-control" type="text" required name="wiratha" id="wiratha" placeholder="Link Sertifikat Wiratha">
+                    <input class="form-control" type="file" accept=".pdf" required name="wiratha" id="wiratha" placeholder="Link Sertifikat Wiratha">
                     <small id="fileHelp" class="form-text text-muted">Kirim dalam link google drive</small>
                 </div>
                 <div class="form-group">
                     <label for="porto">Portofolio (opsional):</label>
-                    <input class="form-control" type="text" name="porto" id="porto" placeholder="Link Portofolio">
+                    <input class="form-control" type="file" accept=".pdf" name="porto" id="porto" placeholder="Link Portofolio">
                     <small id="fileHelp" class="form-text text-muted">Kirim dalam link google drive</small>
                 </div>
                 <div class="mt-3">
