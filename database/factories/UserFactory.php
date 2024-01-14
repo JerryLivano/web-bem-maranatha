5d7387f41f3b0b7a08a2f720edbf7ae327a07212
Christopher Richard <christorichard83@gmail.com>
2024-01-14 22:20:46 +0700

    jeri imut

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'nrp'=>mt_rand(2172000,2172100),
+            'nrp'=>mt_rand(2172000,2572100),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'password' => static::$password ??= Hash::make('password'),
